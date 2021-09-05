a892a091bb2f18c2d255e28466e8801ea6c654e9
EnderXiao <1286554811@qq.com>
2021-09-05 17:59:14 +0800

    2021/9/5

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,2 +1,93 @@
+# Hello.py
+# -*- coding: utf-8 -*-
+from bs4.element import CData, NavigableString
+import requests
+import copy
+from bs4 import BeautifulSoup
+
+html_doc_test = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+
+
+requestHeader = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)  Mobile/15E148 wxwork/3.1.12 MicroMessenger/7.0.1 Language/zh ColorScheme/Light',
+    'Connection': 'keep-alive',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
+    'X-Requested-With': 'XMLHttpRequest',  # Ajax异步请求
+}
+
+
+def setRequestHeader(**kwargs):
+    header = copy.deepcopy(requestHeader)
+    for key, value in kwargs:
+        header[key] = value
+    return header
+
+
+def myRequest(type, url, **kwargs):
+    setRequestHeader(**kwargs)
+    myRespond = None
+    if type.lower() == 'get':
+        myRespond = requests.get(url, **kwargs)
+    elif type.lower() == 'post':
+        myRespond = requests.post(url, **kwargs)
+    return myRespond
+
+
 if __name__ == "__main__":
-    print("Hello")
+    html_doc = myRequest("get", "http://quotes.toscrape.com/").text
+    soup = BeautifulSoup(html_doc, "lxml")
+    tag = soup.title
+    print(soup.prettify())  # 输出格式化后的html
+    print('---------分界线---------')
+    print(soup.title.parent)
+    soup.title.name = "fuck"  # 修改标签名
+    print('-----分界线-----')
+    print(tag)
+    css_soup = BeautifulSoup("<p class='fuck shit'></p>", 'lxml')
+    print(css_soup.p['class'])
+    rel_soup = BeautifulSoup('<p>Back to the <a rel="index">homepage</a></p>','lxml')
+    rel_soup.a['rel'] = ['index', 'contents']
+    print(rel_soup.a['rel'])
+    print(rel_soup.p)
+    print(rel_soup.a.string)
+    print(type(rel_soup.a.string))
+    print(type(str(rel_soup.a.string)))
+    tag = rel_soup.a
+    print(tag.string)
+    tag.string.replace_with('hhhhh')
+    print(css_soup.name)
+    print(css_soup.attrs)
+    print('-----分界线-----')
+    doc = BeautifulSoup("<head>INSERT FOOTER HERE</head>", "xml")
+    foot = BeautifulSoup("<footer>Here is the footer</footer>", "xml")
+    doc.head.string.replace_with(foot)
+    print(doc)
+    print('-----分界线-----')
+    markup = "<b><!-- Hey, buddy. Want to buy a used parser? --></b>"
+    soup = BeautifulSoup(markup, "lxml")
+    comment = soup.b.string
+    print(type(comment))
+    print(comment)
+    print(isinstance(comment, NavigableString))
+    print(soup.prettify())
+    cdata = CData("A CDATA block <")
+    comment.replace_with(cdata)
+    print(soup.prettify())
+    print("-----分界线-----")
+    soup_test = BeautifulSoup(html_doc_test, 'lxml')
+    tag = soup_test.body
+    print(tag.contents)
+    print(tag.contents[1].contents[0].contents)
+    print(soup_test.contents)
---
+++ YQSB/YQSB.py
@@ -0,0 +1,109 @@
+import time
+import json
+import requests
+import copy
+
+requestHeader = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)  Mobile/15E148 wxwork/3.1.12 MicroMessenger/7.0.1 Language/zh ColorScheme/Light',
+    'Connection': 'keep-alive',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
+    'X-Requested-With': 'XMLHttpRequest',  # Ajax异步请求
+}
+
+
+def myRequest(type,session: requests.sessions.session, url, referer = None, accept = None, **kwargs):
+    headers = copy.deepcopy(requestHeader)
+    if isinstance(referer, str):
+        headers['Referer'] = referer
+    if isinstance(accept, str):
+        headers['Accept'] = accept
+    s.headers.update(headers)
+    if type == 'get':
+        getRequest = session.get(url, **kwargs)
+        return getRequest
+    if type == 'post':
+        postRequest = session.post(url, **kwargs)
+        return postRequest
+
+
+if __name__ == '__main__':
+    s = requests.session();
+    s.cookies.set('id','402880c97b5d8ad1017b9044b9a12713')
+    s.cookies.set('token','1C062C6EC563BF3582A0473BDF4D3CC4')
+
+    # 请求企业微信session
+    res = myRequest('get', s, url='http://bjut.sanyth.com:81/nonlogin/qywx/authentication.htm?appId=402880c97b1aa5f7017b1ad2bd97001b&urlb64=L3dlaXhpbi9zYW55dGgvaG9tZS5odG1s',
+                    accept='text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+    # 请求xmid
+    data = {
+        "pageIndex" : "0",
+        "pageSize" : "10",
+        "type" : "YQSJSB",
+    }
+    res = myRequest('post', s, url='http://bjut.sanyth.com:81/syt/zzapply/queryxmqks.htm',
+                    referer='http://bjut.sanyth.com:81/webApp/xuegong/index.html',
+                    accept='application/json, text/plain, */*',data=data)
+    jsonRespons = json.loads(res.content)
+    xmid = jsonRespons['data'][0]['id']
+    # 请求PDNF
+    data = {
+        "type":"YQSJSB"
+    }
+    res = myRequest('post', s, url='http://bjut.sanyth.com:81/syt/zzapi/getPDNF.htm',
+                    referer='http://bjut.sanyth.com:81/webApp/xuegong/index.html',
+                    accept='application/json, text/plain, */*',data = data)
+    pdnf = res.text
+    # 请求签到状态
+    data={
+        "xmid":xmid,
+        "pdnf":pdnf,
+        "type":"YQSJSB"
+    }
+    res = myRequest('post', s, url="http://bjut.sanyth.com:81/syt/zzapply/checkrestrict.htm",
+                    referer='http://bjut.sanyth.com:81/webApp/xuegong/index.html',
+                    accept='application/json, text/plain, */*',data = data)
+    logMessage = "打卡失败"
+    if "今日已经上报" not in res.text:
+        # 打卡请求
+        c1 = "21级研究生新生"
+        c2 = "在校外居住"
+        c3 = "今日入校"
+        c4 = "否"
+        c5 = "正常"
+        c6 = "正常"
+        c7 = "无情况"
+        c8 = "在京内"
+        c9 = "否"
+        c10 = "否"
+        c11 = "否"
+        c12 = "北京市,北京市,朝阳区,大望路27号中蓝学生公寓"
+        location_longitude = 116.48051452636719
+        location_latitude = 39.88443374633789
+        location_address = "北京市朝阳区南磨房镇南磨房路平乐园小区"
+        data = {
+            "data" : '{"xmqkb":{"id":"%s"},"c1":"%s","c2":"%s","c3":"%s","c4":"%s","c5":"%s","c6":"%s","c7":"%s","c8":"%s","c12":"%s","c9":"%s","c10":"%s","c11":"%s","type":"YQSJSB","location_longitude":%s,"location_latitude":%s,"location_address":"%s"}' % (xmid,c1,c2,c3,c4,c5,c6,c7,c8,c12,c9,c10,c11,location_longitude,location_latitude,location_address),
+            "msgurl" : "syt/zzapply/list.htm?type=YQSJSB&xmid=%s" % (xmid),
+            "uploadFileStr" : {},
+            "multiSelectData" : {},
+            "type":"YQSJSB"
+        }
+        print(data)
+        res = myRequest("post", s, url="http://bjut.sanyth.com:81/syt/zzapply/operation.htm",
+                        referer='http://bjut.sanyth.com:81/webApp/xuegong/index.html',
+                        accept='application/json, text/plain, */*',data = data)
+        if "success" in res.text:
+            logMessage = "打卡成功"
+            print("打卡成功")
+        else:
+            logMessage = "打卡请求失败"
+            print("打卡请求失败")
+    else:
+        print('今日已打卡')
+        logMessage = "今日已打卡"
+    requestRecord = res.text
+    requestTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    logText = requestTime + ' ' + logMessage + ' ' + res.text +  '\n'
+    with open("COVID19Report2021.txt",'a') as f:
+        f.writelines(logText)
